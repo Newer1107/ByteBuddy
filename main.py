7ad6a03e60f047eb4d339e7b9e475551a17cd1df
Newer1107 <newerxd1@gmail.com>
2023-07-08 19:22:39 +0530

    Updated Bot.

--- main.py
+++ main.py
@@ -1,46 +1,72 @@
 import discord
 import os
 import subprocess
-from discord.ext import commands
+import datetime
+import psutil
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+LOG_FILE = os.getenv('LOG_FILE_LOCATION')
 current_directory = '/home/'
-# Define your desired intents
 intents = discord.Intents.all()
-CHANNEL_ID = ADD-CHANNEL-ID-HERE
-# Create a new bot instance with the intents argument
 bot = commands.Bot(command_prefix='!', intents=intents)
-# Event: When the bot is ready and connected to Discord
+
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+    print(f'Successfully Logged in as {bot.user.name} ({bot.user.id})')
     print('------')
-
-# Command: Ping-Pong
+    update_uptime.start()
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
 
-# Command: Say Hello
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hello, {ctx.author.name}!')
+
+@bot.command()
+async def uptime(ctx):
+    uptime = get_system_uptime()
+    await ctx.send(f'Hello {ctx.author.name}, your server uptime is `{uptime}`!')
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+
+    if isinstance(error, commands.CommandInvokeError):
+        original_error = error.original
+        if isinstance(original_error, discord.errors.HTTPException):
+            if "Invalid Form Body" in str(original_error):
+                await ctx.send("Command output is too long to display.")
+                return
+
+        raise error
+
 @bot.event
 async def on_message(message):
-    global current_directory  # Access the global current_directory variable
+    if message.author.bot:
+        return
 
-    if message.channel.id == CHANNEL_ID and not message.author.bot:
+    if message.content.startswith(bot.command_prefix):
+        await bot.process_commands(message)
+    elif message.channel.id == CHANNEL_ID:
         command = message.content.strip()
+        output = execute_command(command)
+        output_message = f'Command: `{command}`\nOutput:\n```\n{output}\n```'
+        if len(output_message) > 2000:
+            output_message = f'{username} Command: `{command}`\nOutput is too long to display.'
+        await message.channel.send(output_message)
 
-        if command == 'clear':
-            await clear_channel_messages(message.channel)
-        else:
-            output = execute_command(command)
-            await message.channel.send(f'Command: `{command}`\nOutput:\n```\n{output}\n```')
-
-async def clear_channel_messages(channel):
-    await channel.purge(limit=None)
+        # Log the command and output to a file
+        log_entry = f'{datetime.datetime.now()}\n\n Command:\n {command}\nOutput:\n {output}\n\n'
+        log_to_file(log_entry)
 
 def execute_command(command):
-    global current_directory  # Access the global current_directory variable
+    global current_directory
 
     try:
         if command.startswith('cd'):
@@ -57,7 +83,19 @@ def execute_command(command):
     except subprocess.CalledProcessError as e:
         return f'Command failed with exit code {e.returncode}.\nError: {e.stderr}'
 
+def log_to_file(log_entry):
+    with open(LOG_FILE, 'a') as file:
+        file.write(log_entry)
+@tasks.loop(minutes=5)  # Update every 5 minutes, adjust the interval as needed
+async def update_uptime():
+    uptime = get_system_uptime()
+    activity = discord.Activity(type=discord.ActivityType.watching, name=f"Uptime: {uptime}")
+    await bot.change_presence(activity=activity)
 
-# Run the bot using your bot token
-bot.run('BOT-TOKEN')
+def get_system_uptime():
+    uptime = datetime.datetime.now() - datetime.datetime.fromtimestamp(psutil.boot_time())
+    days, hours, minutes, seconds = uptime.days, uptime.seconds // 3600, (uptime.seconds // 60) % 60, uptime.seconds % 60
+    return f"{days}d {hours}h {minutes}m {seconds}s"
 
+
+bot.run(BOT_TOKEN)
